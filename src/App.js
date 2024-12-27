9c30c71102314395e3cfd04775c56bb38c7f3700
neha0193 <neha031919@gmail.com>
2024-12-27 21:11:11 +0530

    another-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import logo from './panda.png';
 import './App.css';
+import loading from './loadingg.gif';
+import Output from './Output';
+import { useState,useEffect } from 'react';
+
+function Header() {
+  return (
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      <img src={loading} className="loading" alt="loading" />
+    </header>
+  )
+}
 
 function App() {
+  const [message, setMessage] = useState(true);
+
+  useEffect(() => {
+    // Set a timeout to change the message after 3 seconds
+    const timer = setTimeout(() => {
+      setMessage(false);
+    }, 5000);
+
+    // Cleanup function to clear the timeout if the component unmounts
+    //return () => clearTimeout(timer);
+  }, []); // Empty dependency array ensures this runs only once
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        {message?<Header/>:<Output/>}
     </div>
   );
 }
---
+++ src/Output.js
@@ -0,0 +1,9 @@
+import logos from './night.jpg';
+function Output () {
+    return (<div className='output'>
+      <img src={logos} alt="night" class="responsive"/>
+      </div>
+    )
+
+}
+export default Output;
